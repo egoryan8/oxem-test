18a1af808790d4aa5f6e5431e13547ce8666c567
Fogel Egor <fogel_92@bk.ru>
2022-10-03 17:17:24 +0700

    feat: add digital regexp, error handler

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import { calcMonthPay } from '../helpers/calcMonthPay';
-import { calcSum } from '../helpers/calcSum';
-import { handleSendRequest } from '../helpers/handleSendRequest';
-import { numberWithSpaces } from '../helpers/numberWithSpaces';
+import { calcMonthPay } from '../utils/calcMonthPay';
+import { calcSum } from '../utils/calcSum';
+import { handleSendRequest } from '../utils/handleSendRequest';
+import { numberWithSpaces } from '../utils/numberWithSpaces';
 
 import '../scss/CardCost.scss';
 import '../scss/CardRange.scss';
@@ -15,15 +15,17 @@ const PERCENTS_MIN = 10;
 const PERCENTS_MAX = 60;
 const LEASING_MIN = 1;
 const LEASING_MAX = 60;
+const DIGIT_REGEXP = /^\d+$/;
 
 const Form = () => {
   const [carPrice, setCarPrice] = useState(3300000);
-  const [percents, setPercents] = useState(10);
+  const [percents, setPercents] = useState(13);
   const [leasing, setLeasing] = useState(60);
   const [initial, setInitial] = useState(Math.round((percents / 100) * carPrice));
   const [monthPay, setMonthPay] = useState(calcMonthPay(carPrice, initial, leasing));
   const [sum, setSum] = useState(calcSum(initial, leasing, monthPay));
   const [isDisabled, setIsDisabled] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const onBlurChange = (e, min, max, setValue) => {
     if (e.target.value > max) {
@@ -43,53 +45,70 @@ const Form = () => {
   };
 
   const onChangePrice = (e) => {
-    const initialValue = Math.round((percents / 100) * e.target.value);
-    setCarPrice(e.target.value);
-    setInitial(initialValue);
-    const month = calcMonthPay(e.target.value, initialValue, leasing);
-    setMonthPay(month);
-    setSum(calcSum(initialValue, leasing, month));
+    if (DIGIT_REGEXP.test(e.target.value) === true || e.target.value === '') {
+      setIsError(false);
+      const initialValue = Math.round((percents / 100) * e.target.value);
+      setCarPrice(e.target.value);
+      setInitial(initialValue);
+      const month = calcMonthPay(e.target.value, initialValue, leasing);
+      setMonthPay(month);
+      setSum(calcSum(initialValue, leasing, month));
+    } else {
+      setCarPrice((prevState) => prevState);
+      setIsError(true);
+    }
   };
 
   const onChangeInitial = (e) => {
-    const percents = Math.round((e.target.value / carPrice) * 100);
-    setInitial(e.target.value);
-    setPercents(percents);
-    const month = calcMonthPay(carPrice, e.target.value, leasing);
-    setMonthPay(month);
-    setSum(calcSum(e.target.value, leasing, month));
+    if (DIGIT_REGEXP.test(e.target.value) === true || e.target.value === '') {
+      setIsError(false);
+      const percents = Math.round((e.target.value / carPrice) * 100);
+      setInitial(e.target.value);
+      setPercents(percents);
+      const month = calcMonthPay(carPrice, e.target.value, leasing);
+      setMonthPay(month);
+      setSum(calcSum(e.target.value, leasing, month));
+    } else {
+      setInitial((prevState) => prevState);
+      setIsError(true);
+    }
   };
 
   const onChangeLeasing = (e) => {
-    setLeasing(e.target.value);
-    const month = calcMonthPay(carPrice, initial, e.target.value);
-    setMonthPay(month);
-    setSum(calcSum(initial, e.target.value, month));
+    if (DIGIT_REGEXP.test(e.target.value) === true || e.target.value === '') {
+      setIsError(false);
+      setLeasing(e.target.value);
+      const month = calcMonthPay(carPrice, initial, e.target.value);
+      setMonthPay(month);
+      setSum(calcSum(initial, e.target.value, month));
+    } else {
+      setLeasing((prevState) => prevState);
+      setIsError(true);
+    }
   };
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
     setIsDisabled(true);
-    handleSendRequest({ carPrice, initial, percents, leasing, monthPay, sum }).then((res) => {
+    handleSendRequest({ carPrice, initial, percents, leasing, monthPay, sum }).then(() => {
       setIsDisabled(false);
     });
   };
 
   return (
     <form className="cards">
+      {isError && <span className="error">Вы ввели не цифру!</span>}
       <div className="card-range">
         <h2 className="card-range__title">Стоимость автомобиля</h2>
         <div className="input-range__wrapper">
           <div className="input-range__value-wrapper">
             <input
-              pattern="[A-Za-z]{3}"
               className="input-range__value input-range__value_input"
-              value={carPrice}
+              value={carPrice || ''}
               onChange={onChangePrice}
-              onBlur={(e) => {
-                onBlurChange(e, CAR_PRICE_MIN, CAR_PRICE_MAX, setCarPrice, onChangePrice);
-              }}
+              onBlur={(e) => onBlurChange(e, CAR_PRICE_MIN, CAR_PRICE_MAX, setCarPrice)}
               disabled={isDisabled}
+              maxLength="7"
             />
             <span className="input-range__value">&#8381;</span>
           </div>
@@ -98,7 +117,7 @@ const Form = () => {
             type="range"
             min={CAR_PRICE_MIN}
             max={CAR_PRICE_MAX}
-            value={carPrice}
+            value={carPrice || ''}
             onChange={onChangePrice}
             step="1000"
             disabled={isDisabled}
@@ -111,13 +130,10 @@ const Form = () => {
           <div className="input-range__value-wrapper">
             <input
               className="input-range__value input-range__value_input"
-              value={initial}
+              value={initial || ''}
               onChange={onChangeInitial}
-              onBlur={(e) => {
-                onBlurChange(e, PERCENTS_MIN, PERCENTS_MAX, setPercents);
-                setInitial();
-              }}
               disabled={isDisabled}
+              maxLength="7"
             />
             <span className="input-range__percents">{percents}%</span>
           </div>
@@ -126,7 +142,7 @@ const Form = () => {
             type="range"
             min={PERCENTS_MIN}
             max={PERCENTS_MAX}
-            value={percents}
+            value={percents || ''}
             onChange={onChangePercents}
             step="1"
             disabled={isDisabled}
@@ -139,10 +155,11 @@ const Form = () => {
           <div className="input-range__value-wrapper">
             <input
               className="input-range__value input-range__value_input"
-              value={leasing}
+              value={leasing || ''}
               onChange={onChangeLeasing}
               onBlur={(e) => onBlurChange(e, LEASING_MIN, LEASING_MAX, setLeasing)}
               disabled={isDisabled}
+              maxLength="2"
             />
             <span className="input-range__value">мес.</span>
           </div>
@@ -151,7 +168,7 @@ const Form = () => {
             type="range"
             min={LEASING_MIN}
             max={LEASING_MAX}
-            value={leasing}
+            value={leasing || ''}
             onChange={onChangeLeasing}
             disabled={isDisabled}
           />
